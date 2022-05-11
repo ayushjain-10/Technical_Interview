2370132ba02d68df9d14e01e9b1f27c03d183604
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-05-11 04:44:06 -0700

    update

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -9,6 +9,18 @@ To accommodate this, nums1 has a length of m + n, where the first m elements den
 and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 '''
 
+# Example 1:
+'''
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+'''
+
+# Given: nums1, m , nums2, n
+# Output: nums1
+
+
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         # Run loop till we check all the numbers in nums1 and nums2
